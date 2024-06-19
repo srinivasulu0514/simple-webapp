7f832a93d6896305f2bf06feec8daed8ae6b0823
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-06-19 14:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Hi sir, Done with my Work, Thank you!"
+    return "Hi sir, Done with my Work, Thank you ('!') "
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
