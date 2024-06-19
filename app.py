32f865d1c2fc696509d3cb9deb0b8292301ffea1
srinivasulu0514 <56268088+srinivasulu0514@users.noreply.github.com>
2024-06-19 16:18:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    a=10
+    return "Hello"
+    '''a=10
     b=20
     print ("The sum is:",a+b)
-main()
+main()'''
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
